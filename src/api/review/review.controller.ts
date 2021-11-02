6f992ab067ffefc9c4986d7ad43720e30b9d90ca
Darshil Khokhar <darshilk@codesquote.com>
2021-11-02 10:57:19 +0530

    Edit review file

--- src/api/review/review.controller.ts
+++ src/api/review/review.controller.ts
@@ -52,81 +52,81 @@ export default class ReviewController {
   }
 
   //To Get Rating & Reviews
-  // @Get("/cookie-maker/review", { transformRequest: true })
-  // @UseBefore(CookieMakerValidator)
-  // async GetReview(@Req() req: IExtendedRequest, @Res() res: IExtendedResponse) {
-  //   try {
-  //     const restaurantId = req.context?.user.restaurantId;
-  //     let query: { [key: string]: any } = {
-  //       restaurantId: restaurantId,
-  //     };
+  @Get("/cookie-maker/review", { transformRequest: true })
+  @UseBefore(CookieMakerValidator)
+  async GetReview(@Req() req: IExtendedRequest, @Res() res: IExtendedResponse) {
+    try {
+      const restaurantId = req.context?.user.restaurantId;
+      let query: { [key: string]: any } = {
+        restaurantId: restaurantId,
+      };
 
-  //     const reviews = await this._reviewService.find(query, {});
+      const reviews = await this._reviewService.find(query, {});
 
-  //     let totalReviews = reviews.length;
+      let totalReviews = reviews.length;
 
-  //     const projection = {
-  //         reviewDate: 1,
-  //         rating: 1,
-  //         reviewComment: 1,
-  //         cookieMonsterId: 1,
-  //       },
-  //       QueryOptions = {
-  //         lean: true,
-  //         sort: { reviews: -1 },
-  //         limit: 5,
-  //         populate: { path: "cookieMonsterId", select: "firstName" },
-  //       },
-  //       review = await this._reviewService.findPopular(
-  //         query,
-  //         projection,
-  //         QueryOptions
-  //       );
+      const projection = {
+          reviewDate: 1,
+          rating: 1,
+          reviewComment: 1,
+          cookieMonsterId: 1,
+        },
+        QueryOptions = {
+          lean: true,
+          sort: { reviews: -1 },
+          limit: 5,
+          populate: { path: "cookieMonsterId", select: "firstName" },
+        },
+        review = await this._reviewService.findPopular(
+          query,
+          projection,
+          QueryOptions
+        );
 
-  //     let averageRatings = 0,
-  //       Ratings = 0,
-  //       awesome = 0,
-  //       good = 0,
-  //       meh = 0,
-  //       bad = 0,
-  //       terrible = 0;
-  //     for (let i = 0; i < reviews.length; i++) {
-  //       Ratings = Ratings + reviews[i].rating;
-  //       averageRatings = Ratings / reviews.length;
-  //       if (reviews[i].rating === 5) {
-  //         awesome++;
-  //       }
-  //       if (reviews[i].rating === 4) {
-  //         good++;
-  //       }
-  //       if (reviews[i].rating === 3) {
-  //         meh++;
-  //       }
-  //       if (reviews[i].rating === 2) {
-  //         bad++;
-  //       }
-  //       if (reviews[i].rating === 1) {
-  //         terrible++;
-  //       }
-  //     }
+      let averageRatings = 0,
+        Ratings = 0,
+        awesome = 0,
+        good = 0,
+        meh = 0,
+        bad = 0,
+        terrible = 0;
+      for (let i = 0; i < reviews.length; i++) {
+        Ratings = Ratings + reviews[i].rating;
+        averageRatings = Ratings / reviews.length;
+        if (reviews[i].rating === 5) {
+          awesome++;
+        }
+        if (reviews[i].rating === 4) {
+          good++;
+        }
+        if (reviews[i].rating === 3) {
+          meh++;
+        }
+        if (reviews[i].rating === 2) {
+          bad++;
+        }
+        if (reviews[i].rating === 1) {
+          terrible++;
+        }
+      }
 
-  //     averageRatings = Math.ceil(averageRatings);
-  //     const Reviews = {
-  //         totalReviews,
-  //         review,
-  //       },
-  //       Rating = {
-  //         averageRatings,
-  //         awesome,
-  //         good,
-  //         meh,
-  //         bad,
-  //         terrible,
-  //       };
+      averageRatings = Math.ceil(averageRatings);
+      const Reviews = {
+          totalReviews,
+          review,
+        },
+        Rating = {
+          averageRatings,
+          awesome,
+          good,
+          meh,
+          bad,
+          terrible,
+        };
 
-  //     return res.formatter.ok({ Rating, Reviews }, true, "RATING_AND_REVIEWS");
-  //   } catch (err) {
-  //     return res.formatter.error({}, false, "ERROR!", err as Error);
-  //   }
-  // }
+      return res.formatter.ok({ Rating, Reviews }, true, "RATING_AND_REVIEWS");
+    } catch (err) {
+      return res.formatter.error({}, false, "ERROR!", err as Error);
+    }
+  }
 }
